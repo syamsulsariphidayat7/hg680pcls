a7964c64e77a2b3bd2f48cd2488c5b91924a5cbb
Syamsul Sarip Hidayat <38783840+syamsulsariphidayat7@users.noreply.github.com>
2025-10-08 11:41:21 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,13 +5,15 @@
 
 echo "[HG680PCLS] Removing installation..."
 
+# Hentikan service
 /etc/init.d/hg680pcls stop 2>/dev/null
 /etc/init.d/hg680pcls disable 2>/dev/null
 
+# Hapus semua file
 rm -f /etc/config/hg680pcls
 rm -f /etc/init.d/hg680pcls
 rm -f /usr/bin/hg680pcl
 rm -f /usr/lib/lua/luci/controller/hg680pcls.lua
 rm -f /usr/lib/lua/luci/model/cbi/hg680pcls.lua
 
-echo "[HG680PCLS] Uninstallation complete!"
+echo "[HG680PCLS] ✅ Uninstallation complete!"
